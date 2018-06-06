5575ecb93847ceee902b3201c71c97a9be4968b5
‘Michael <michael.daniels.jr@gmail.com>
2018-06-06 14:06:07 -0700

    history saving to table

--- controllers/lobcontroller.js
+++ controllers/lobcontroller.js
@@ -13,33 +13,52 @@ module.exports = {
     let htmlContent = theDocument.content.replace(/\n\n/g, '<br /><div style="margin:25px 0"></div>');
 
     Lob.letters.create({
-  description: 'Demo Letter',
-  to: {
-    name: recipient.contactName,
-    address_line1: recipient.streetAddress,
-    address_line2: recipient.street2,
-    address_city: recipient.city,
-    address_state: recipient.state,
-    address_zip: recipient.zip,
-    address_country: 'US',
-  },
-  from: {
-    name: 'Mailmate',
-    address_line1: '8031 W Hilton Ave',
-    address_line2: '',
-    address_city: 'Phoenix',
-    address_state: 'AZ',
-    address_zip: '85043',
-    address_country: 'US',
-  },
-  file: `<html style="padding-top: 3in; margin: .5in; font-size:12px">${htmlContent}</html>`,
-  color: true
-  }, function (err, res) {
-    console.log(err, res);
-  })
-  .then(() => {
-    console.log(res)
-    res.send("Got it")
+      description: 'Demo Letter',
+      to: {
+        name: recipient.contactName,
+        address_line1: recipient.streetAddress,
+        address_line2: recipient.street2,
+        address_city: recipient.city,
+        address_state: recipient.state,
+        address_zip: recipient.zip,
+        address_country: 'US',
+      },
+      from: {
+        name: 'Mailmate',
+        address_line1: '8031 W Hilton Ave',
+        address_line2: '',
+        address_city: 'Phoenix',
+        address_state: 'AZ',
+        address_zip: '85043',
+        address_country: 'US',
+      },
+      file: `<html style="padding-top: 3in; margin: .5in; font-size:12px">${htmlContent}</html>`,
+      color: true
+      }, function (err, res) {
+        console.log(err, res);
+    })
+  .then((response) => {
+    knex('history')
+      .insert({
+        date_created: response.date_created,
+        mail_type: response.mail_type,
+        carrier: response.carrier,
+        tracking_number: response.tracking_number,
+        img_url: response.url,
+        small_thumbnail: response.thumbnails[0].small,
+        medium_thumbnail: response.thumbnails[0].medium,
+        expected_delivery_date: response.expected_delivery_date,
+        recipient_name: response.to.name,
+        recipient_street: response.to.address_line1,
+        recipient_street2: response.to.address_line2,
+        recipient_city: response.to.address_city,
+        recipient_state: response.to.address_state,
+        recipient_zip: response.to.address_zip,
+        user_id: recipient.user_id
+      })
+      .then(() => {
+        res.sendStatus('200')
+      })
   })
   },
 
